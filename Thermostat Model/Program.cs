f6a25101246913db8bcd621849d717b405f00aa6
slav6897337 <slava6897337@gmail.com>
2020-11-07 19:27:11 +0300

    Thermostat

--- Thermostat Model/Program.cs
+++ Thermostat Model/Program.cs
@@ -15,6 +15,8 @@
             Thermostat thermostat = new Thermostat();
             thermostat.TemperatureChanging += HeatingUnit.Heating;
             thermostat.TemperatureChanging += CoolingUnit.Cooling;
+            ITemperatureChanges temperatureChanges = thermostat;
+            temperatureChanges.TemperatureChanges(50);
             thermostat.TemperatureChanges(90);
             thermostat.TemperatureChanges(40);
             thermostat.Liquid.Temperature = 20;
